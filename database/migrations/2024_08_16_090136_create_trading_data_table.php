a68d2bf54d4caec74bf7e2affda03f08d69cd9fd
learnearnplay07@gmail.com <youremail@example.com>
2025-04-26 08:10:19 +0800

    Version 1

--- database/migrations/2024_08_16_090136_create_trading_data_table.php
+++ database/migrations/2024_08_16_090136_create_trading_data_table.php
@@ -4,13 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePropfirmTable extends Migration
+class CreateTradingDataTable extends Migration
 {
     public function up()
     {
         Schema::create('propfirm', function (Blueprint $table) {
             $table->id();
-            $table->string('prop_firm');
+            $table->string('propfirm');
             $table->string('capital');
             $table->decimal('price', 10, 2);
             $table->decimal('discount', 5, 2);
